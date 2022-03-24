11b10eeb2cc01485621eea61584271b3b802de51
anjobaiju <101249617+anjobaiju@users.noreply.github.com>
2022-03-24 23:23:59 +0530

    Input your name

---
+++ C++ basics 02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char name[50];
+   cout << "Please enter your name: ";
+   cin >> name;
+   cout << "Your name is: " << name << endl;
+}
